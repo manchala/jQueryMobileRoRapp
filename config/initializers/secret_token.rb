bae1d6431bafbfbbc292116a8bd7e74e57d7ebac
manchala <vinita_manchala@hotmail.com>
2012-05-08 09:43:33 -0700

    A jquery mobile app to display blog messages on an iPod/iPhone/iPad device. It uses a Ruby on Rails with SQLite on the back end. To test locally run it on WEBrick with an IP address (say, 192.168.1.21, but not 127.0.0.1), point your iPod Safari to this address.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmoblog::Application.config.secret_token = '3039002dcbac2b241de295dee67a89f7ec632b12c5e7ee61be514da7330738681312c16da76a44ab998abc1e51f2110ba43f86be0daba04d85042e3253182c9a'
